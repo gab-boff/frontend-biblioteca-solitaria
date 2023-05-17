c0d99bf5f43d6655802714f4553c74f5afe1216e
gab-boff <gmeninboff@gmail.com>
2023-05-17 20:44:15 -0300

    router base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
-  return <>Oi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
+
+export default routes;
